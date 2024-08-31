7904235c98d3aa2b3103e221e9ad4726b65ba0f0
Monirul Jim <monirulislamjim6640@gmail.com>
2024-09-01 00:23:42 +0600

    doctor relationship

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class DoctorSpecializationAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug': ['name',]}
+    
+class DoctorDesignationAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug': ['name',]}
+    
+    
+admin.site.register(models.DoctorSpecialization,DoctorSpecializationAdmin)
+admin.site.register(models.DoctorDesignation,DoctorDesignationAdmin)
+admin.site.register(models.DoctorAvailableTime)
+admin.site.register(models.DoctorModel)
--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,40 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class DoctorSpecialization(models.Model):
+    
+    name=models.CharField(max_length=30)
+    slug=models.SlugField(max_length=30)
+    def __str__(self):
+        return self.name
+    
+    
+class DoctorDesignation(models.Model):
+    
+    name=models.CharField(max_length=30)
+    slug=models.SlugField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class DoctorAvailableTime(models.Model):
+    
+    name=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class DoctorModel(models.Model):
+    
+    name=models.OneToOneField(User,on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='doctor/image')
+    designation=models.ManyToManyField(DoctorDesignation)
+    specialization=models.ManyToManyField(DoctorSpecialization)
+    available_time=models.ManyToManyField(DoctorAvailableTime)
+    fee=models.IntegerField()
+    meet_link=models.CharField(max_length=100)
+    
+    
+    def __str__(self):
+        return f' {self.name.first_name} {self.name.last_name}'
